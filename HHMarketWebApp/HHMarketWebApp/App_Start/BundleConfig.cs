fe3c9adc3c2ec426617a6391811080ac55320dc1
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2018-10-03 23:32:41 -0700

    update Login

--- HHMarketWebApp/HHMarketWebApp/App_Start/BundleConfig.cs
+++ HHMarketWebApp/HHMarketWebApp/App_Start/BundleConfig.cs
@@ -24,7 +24,9 @@ namespace HHMarketWebApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/hhstyle.css",
+                      "~/Content/Header-Nightsky.css"));
         }
     }
 }
--- HHMarketWebApp/HHMarketWebApp/App_Start/RouteConfig.cs
+++ HHMarketWebApp/HHMarketWebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace HHMarketWebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Login", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
