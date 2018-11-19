0a0867db07d79b9fab5791411968ce0e06eeaee3
ntthuong <thln2003@yahoo.com>
2018-11-18 20:23:13 -0600

    Register user screen

---
+++ HHMarketWebApp/HHMarketWebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Web.Mvc;
+using HHMarketWebApp.Models;
+using FormsAuth;
+
+namespace HHMarketWebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+       private DBModelContainer db = new DBModelContainer();
+
+        //GET
+        public ActionResult Index(User user)
+        {
+            return View(user);
+        }
+
+        // POST 
+        [HttpPost]
+        public ActionResult RegisterUser([Bind(Include = "UserName,Password,Email, City, FirstName, LastName, State,Zipcode, Address")]User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                Logon logon = new Logon();
+                logon.Username = user.UserName;
+                logon.Password = user.Password;
+                if (user.UserId > 0 && UserManager.ValidateUser(logon, Response))
+                {
+                    ViewBag.LoginFailure = 0;
+                    ModelState.Clear();
+                    return RedirectToAction("Index", "Category");
+
+                }
+                ModelState.Clear();
+              
+            }
+            ViewBag.LoginFailure = 1;
+            return RedirectToAction("Index", "Registration", user);
+
+        }
+    }
+}
--- HHMarketWebApp/HHMarketWebApp/Models/User.cs
+++ HHMarketWebApp/HHMarketWebApp/Models/User.cs
@@ -20,31 +20,42 @@ namespace HHMarketWebApp.Models
 
         public int UserId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter an User name")]
+        [Display(Name = "User Name:")]
         public string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter Password")]
+        [Display(Name = "Password:")]
         public string Password { get; set; }
 
-        [Required]
+        [EmailAddress(ErrorMessage = "The Email Address is not valid")]
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [Display(Name = "Email Address:")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter the user's last name.")]
+        [Display(Name = "First Name:")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter the user's last name.")]
+        [Display(Name = "Last Name:")]
         public string LastName { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "Please enter the user's Address.")]
+        [Display(Name = "Address:")]
         public string Address { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter the user's city.")]
+        [Display(Name = "City:")]
         public string City { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter the user's state.")]
+        [Display(Name = "State:")]
         public string State { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter the user's zipcode.")]
+        [Display(Name = "Zipcode:")]
         public string ZipCode { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
