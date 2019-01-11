91126249ee78f45907b8975c56c1f59ae903ce59
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2019-01-10 20:31:18 -0800

    aa

---
+++ HHMarketWebApp/HHMarketWebApp/Models/ReviewDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HHMarketWebApp.Models
+{
+    public class ReviewDetails
+    {
+        public Production product { get; set; }
+
+        public List<ReviewProduction> reviewList { get; set; }
+
+        public Rating rating { get; set; }  
+    }
+}
