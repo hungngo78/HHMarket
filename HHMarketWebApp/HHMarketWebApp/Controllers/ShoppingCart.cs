7f6010b7e98e84e35e99730b91e3e9daa2255c6d
ntthuong <thln2003@yahoo.com>
2019-01-12 11:30:34 -0600

    shopping cart

---
+++ HHMarketWebApp/HHMarketWebApp/Controllers/ShoppingCart.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using HHMarketWebApp.Models;
+using PagedList;
+
+namespace HHMarketWebApp.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public ActionResult Index(int userId)
+        {
+            DBModelContainer db = new DBModelContainer();
+
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.listItemCart = (from c in db.Carts
+                                         join cdetail in db.CartDetails on c.CartId equals cdetail.CartId
+                                         join pdetail in db.ProductDetails on cdetail.ProductDetailsId equals pdetail.ProductDetailsId
+                                         join p in db.Products on pdetail.ProductId equals p.ProductId
+                                         where c.UserId == userId && cdetail.Type == 0
+                                         select new CartDetailItem()
+                                         {
+                                             Amount = cdetail.Amount,
+                                             CartId = cdetail.CartId,
+                                             Type = cdetail.Type,
+                                             ExtendedPrice = cdetail.ExtendedPrice,
+                                             ProductDetailsId = cdetail.ProductDetailsId,
+                                             Price = pdetail.Price,
+                                             Picture = pdetail.Picture,
+                                             ProductName = p.Name,
+                                             TotalAmountProduction = pdetail.Amount
+                                         }).ToList();
+
+            
+
+           
+
+            return View(shoppingCart);
+        }
+
+    }
+}
---
+++ HHMarketWebApp/HHMarketWebApp/Models/CartDetailItem.cs
@@ -0,0 +1,34 @@
+namespace HHMarketWebApp.Models
+{
+
+    public class CartDetailItem
+    {
+     
+        public int CartDetailsId { get; set; }
+
+        public short Amount { get; set; }
+
+        public decimal ExtendedPrice { get; set; }
+
+        public short Type { get; set; }
+
+        public int ProductDetailsId { get; set; }
+
+        public int CartId { get; set; }
+
+        public string Username { get; set; }
+
+        public Cart Cart { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Picture { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int TotalAmountProduction { get; set; }
+
+    }
+}
