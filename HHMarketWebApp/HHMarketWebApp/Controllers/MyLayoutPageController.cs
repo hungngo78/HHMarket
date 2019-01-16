e5a52823772bdc7c03721d6ca2d121cfa11db0c8
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2019-01-16 14:58:42 -0800

    add Search feature

---
+++ HHMarketWebApp/HHMarketWebApp/Controllers/MyLayoutPageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HHMarketWebApp.Models;
+
+namespace HHMarketWebApp.Controllers
+{
+    public class MyLayoutPageController : Controller
+    {
+        // GET: MySearch
+        public ActionResult Index()
+        {
+            DBModelContainer db = new DBModelContainer();
+
+            var categories = from c in db.Categories
+                             select c;
+            categories = categories.OrderBy(c => c.CategoryId).Take(20);
+            
+            ViewBag.categories = categories;
+
+            return PartialView();
+        }
+    }
+}
