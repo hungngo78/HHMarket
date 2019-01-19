94cac459d3328895628b52f8b84fb0ee1dd4c167
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2019-01-18 17:06:36 -0800

    .

---
+++ HHMarketWebApp/HHMarketWebApp/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HHMarketWebApp
+{
+    public static class Config
+    {
+        public const int NEED_LOGIN = 1;
+
+       
+    }
+}
