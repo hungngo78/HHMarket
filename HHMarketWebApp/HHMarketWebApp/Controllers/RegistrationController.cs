7b3e3314cfdda4c1da5c0086dca687364dc74501
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2019-01-20 17:12:39 -0800

    .

--- HHMarketWebApp/HHMarketWebApp/Controllers/RegistrationController.cs
+++ HHMarketWebApp/HHMarketWebApp/Controllers/RegistrationController.cs
@@ -3,53 +3,90 @@ using System.Web.Mvc;
 using HHMarketWebApp.Models;
 using FormsAuth;
 using System.Web;
+using System.Linq;
 
 namespace HHMarketWebApp.Controllers
 {
     public class RegistrationController : Controller
     {
-       private DBModelContainer db = new DBModelContainer();
+        private DBModelContainer db = new DBModelContainer();
 
         //GET
-      //  public ActionResult Index(User user, int? error)
-     //   {
-       //     return View(user);
-//}
+        public ActionResult Index()
+        {
+             return View();
+        }
+
+        //GET
+        public ActionResult Edit()
+        {
+            if (UserManager.User != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = (from u in db.Users
+                                where u.UserId == UserManager.User.Id
+                                select u
+                                ).FirstOrDefault();
+
+                    return View("Index", user);
+                }
+            }
+
+            return RedirectToAction("Index", "Category");
+        }
 
         // POST 
-       // [HttpPost]
-       // [HttpGet]
-        public ActionResult Index([Bind(Include = "UserName,Password,Email, City, FirstName, LastName, State,Zipcode, Address")]User user, int? error)
+        [HttpPost]       
+        public ActionResult Update([Bind(Include = "UserName,Password,Email, City, FirstName, LastName, State,Zipcode, Address")] User _user)
         {
-            ViewBag.LoginFailure = error;
             if (ModelState.IsValid)
             {
-                db.Users.Add(user);
-                db.SaveChanges();
-                Logon logon = new Logon();
-                logon.Username = user.UserName;
-                logon.Password = user.Password;
-                if (user.UserId > 0 && UserManager.ValidateUser(logon, Response))
+                if (UserManager.User != null)
                 {
-                    ViewBag.LoginFailure = 0;
+                    User user = (from u in db.Users
+                                  where u.UserId == UserManager.User.Id
+                                  select u
+                                ).FirstOrDefault();
+                    user.UserName   = _user.UserName;
+                    user.Password   = _user.Password;
+                    user.Email      = _user.Email;
+                    user.City       = _user.City;
+                    user.FirstName  = _user.FirstName;
+                    user.LastName   = _user.LastName;
+                    user.State      = _user.State;
+                    user.ZipCode    = _user.ZipCode;
+                    user.Address    = _user.Address;
+
+                    db.SaveChanges();
+
                     ModelState.Clear();
+
                     return RedirectToAction("Index", "Category");
-
                 }
-                ModelState.Clear();
-              
-            }
-            if (ViewBag.LoginFailure == null)
-            {
-                ViewBag.LoginFailure = 2;
-            }
-            else
-            {
-                ViewBag.LoginFailure = 1;
-            }
-            //return RedirectToAction("Index", "Registration", user);
-            return View(user);
+                else
+                {
+                    db.Users.Add(_user);
+                    db.SaveChanges();
+                
+                    Logon logon = new Logon();
+                    logon.Username = _user.UserName;
+                    logon.Password = _user.Password;
+                    if (_user.UserId > 0 && UserManager.ValidateUser(logon, Response))
+                    {
+                        ModelState.Clear();
 
+                        return RedirectToAction("Index", "Category");
+                    }
+                }
+
+                ModelState.Clear();
+            }
+
+            ViewBag.RegisterFailure = 1;    // errors ocured 
+            return View("Index", _user);
         }
+
+        
     }
 }
